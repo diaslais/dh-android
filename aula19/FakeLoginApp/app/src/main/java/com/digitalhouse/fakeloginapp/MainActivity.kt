3b16ce3e40db45402ea7ebd52e0eeea20fb5cf8f
Lais Dias <laisdias099@gmail.com>
2020-10-19 19:14:49 -0300

    login and welcome activity

--- aula19/FakeLoginApp/app/src/main/java/com/digitalhouse/fakeloginapp/MainActivity.kt
+++ aula19/FakeLoginApp/app/src/main/java/com/digitalhouse/fakeloginapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.digitalhouse.fakeloginapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.digitalhouse.fakeloginapp.users.UserService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,26 @@ class MainActivity : AppCompatActivity() {
         btnCreateAccount.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            if (email.isEmpty()){
+                edtEmail.error = "O e-mail deve ser preenchido."
+            } else if(password.isEmpty()){
+                edtPassword.error = "O password deve ser preenchido."
+            } else {
+                val usuario = UserService.logIn(email, password)
+                if (usuario == null){
+                    Toast.makeText(this, "E-mail ou password inválidos.", Toast.LENGTH_LONG).show()
+                } else {
+                    val intent = Intent(this, Welcome::class.java)
+                    intent.putExtra("NAME", usuario.name)
+                    startActivity(intent)
+                }
+            }
         }
     }
 }
