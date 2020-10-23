98074d2d85ef64ca79a05407b34403acdb1c3624
Lais Dias <laisdias099@gmail.com>
2020-10-23 15:50:34 -0300

    ativando os fragments

--- aula21/exercicio1/app/src/main/java/com/laisd/exercicio1/MainActivity.kt
+++ aula21/exercicio1/app/src/main/java/com/laisd/exercicio1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.laisd.exercicio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnA by lazy { findViewById<Button>(R.id.btnFragmentA) }
+        val btnB by lazy { findViewById<Button>(R.id.btnFragmentB) }
+
+        val manager = supportFragmentManager
+
+        btnA.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.container, AFragment())
+            transaction.commit()
+        }
+
+        btnB.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.container, BFragment())
+            transaction.commit()
+        }
     }
 }
