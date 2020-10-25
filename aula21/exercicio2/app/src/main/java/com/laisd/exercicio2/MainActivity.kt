c8204e635012a3d1cdd9e2120823d48439847e2d
Lais Dias <laisdias099@gmail.com>
2020-10-25 01:20:01 -0300

    layout dos fragments

--- aula21/exercicio2/app/src/main/java/com/laisd/exercicio2/MainActivity.kt
+++ aula21/exercicio2/app/src/main/java/com/laisd/exercicio2/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = supportFragmentManager
+
+        val transaction1 = manager.beginTransaction()
+        transaction1.add(R.id.containerA, AFragment())
+        transaction1.commit()
+
+        val transaction2 = manager.beginTransaction()
+        transaction2.add(R.id.containerB, BFragment())
+        transaction2.commit()
     }
 }
