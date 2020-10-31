81b81388138085509ee85b42e5a42c54493b8538
Lais Dias <laisdias099@gmail.com>
2020-10-31 02:17:56 -0300

    esvaziar laber de erro do TextInputLayout ao digitar algo

--- aula24/meu-app/app/src/main/java/com/laisd/meuapp/SignIn.kt
+++ aula24/meu-app/app/src/main/java/com/laisd/meuapp/SignIn.kt
@@ -2,12 +2,15 @@ package com.laisd.meuapp
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_sign_in.view.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,6 +20,7 @@ private const val ARG_PARAM2 = "param2"
 
 class SignIn : Fragment() {
     lateinit var iLogIn: ILogIn
+    lateinit var signInView: View
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -28,26 +32,63 @@ class SignIn : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val signInView = inflater.inflate(R.layout.fragment_sign_in, container, false)
+        signInView = inflater.inflate(R.layout.fragment_sign_in, container, false)
 
         signInClick(signInView)
+
+        // Esvaziar label de erro no TextInputLayout ao digitar algo
+        signInView.findViewById<TextInputEditText>(R.id.txtEdtUsernameSignIn).addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                signInView.findViewById<TextInputLayout>(R.id.txtLayoutUsernameSIgnIn).error = ""
+            }
+        })
+
+        // Esvaziar label de erro no TextInputLayout ao digitar algo
+        signInView.findViewById<TextInputEditText>(R.id.txtEdtPasswordSignIn).addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                signInView.findViewById<TextInputLayout>(R.id.txtLayouttPasswordSignIn).error = ""
+            }
+        })
 
         return signInView
     }
 
     fun signInClick(signInView: View){
         val user = signInView.findViewById<TextInputEditText>(R.id.txtEdtUsernameSignIn)
+        val userLayout = signInView.findViewById<TextInputLayout>(R.id.txtLayoutUsernameSIgnIn)
         val password = signInView.findViewById<TextInputEditText>(R.id.txtEdtPasswordSignIn)
+        val passwordLayout = signInView.findViewById<TextInputLayout>(R.id.txtLayouttPasswordSignIn)
         val btnLogIn = signInView.findViewById<Button>(R.id.btnLogInSignIn)
 
         btnLogIn.setOnClickListener {
+        var taSemErro = true
             if (user.text.toString().isEmpty()){
-                user.error = "Username is required"
-            } else if (password.text.toString().isEmpty()){
-                password.error = "Password is required"
-            } else {
+                userLayout.error = "Username is required"
+                taSemErro = false
+            }
+            if (password.text.toString().isEmpty()){
+                passwordLayout.error = "Password is required"
+                taSemErro =  false
+            }
+            if (taSemErro) {
                 iLogIn.logIn(user.text.toString(), password.text.toString())
             }
         }
+    }
+
+    fun apagaErro(edt: String, layout: TextInputLayout){
+        layout.isErrorEnabled = edt.isEmpty()
     }
 }
--- aula24/meu-app/app/src/main/java/com/laisd/meuapp/SignUp.kt
+++ aula24/meu-app/app/src/main/java/com/laisd/meuapp/SignUp.kt
@@ -2,6 +2,8 @@ package com.laisd.meuapp
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +27,7 @@ private const val ARG_PARAM2 = "param2"
 
 class SignUp : Fragment() {
     lateinit var iSignUp: ISignUp
+    lateinit var signUpView: View
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -36,32 +39,82 @@ class SignUp : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val signUpView = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        signUpView = inflater.inflate(R.layout.fragment_sign_up, container, false)
         signUpClick(signUpView)
+
+        // Esvaziar label de erro no TextInputLayout ao digitar algo
+        signUpView.findViewById<TextInputEditText>(R.id.txtEdtUsernameSIgnUp).addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                signUpView.findViewById<TextInputLayout>(R.id.txtLayoutUsernameSIgnUp).error = ""
+            }
+        })
+        // Esvaziar label de erro no TextInputLayout ao digitar algo
+        signUpView.findViewById<TextInputEditText>(R.id.txtEdtPasswordSignUp).addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                signUpView.findViewById<TextInputLayout>(R.id.txtLayoutPasswordSignUp).error = ""
+            }
+        })
+        // Esvaziar label de erro no TextInputLayout ao digitar algo
+        signUpView.findViewById<TextInputEditText>(R.id.txtEdtConfirmPasswordSignUp).addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                signUpView.findViewById<TextInputLayout>(R.id.txtLayoutConfirmPasswordSignUp).error = ""
+            }
+        })
+
         return signUpView
     }
 
     fun signUpClick(signUpView: View){
         val btnSignUp = signUpView.findViewById<Button>(R.id.btnSignUpSignUp)
         val checkBox = signUpView.findViewById<CheckBox>(R.id.checkAgreeSignUp)
+        val user = signUpView.findViewById<TextInputEditText>(R.id.txtEdtUsernameSIgnUp)
+        val userLayout = signUpView.findViewById<TextInputLayout>(R.id.txtLayoutUsernameSIgnUp)
+        val password = signUpView.findViewById<TextInputEditText>(R.id.txtEdtPasswordSignUp)
+        val passwordLayout = signUpView.findViewById<TextInputLayout>(R.id.txtLayoutPasswordSignUp)
+        val confirmation = signUpView.findViewById<TextInputEditText>(R.id.txtEdtConfirmPasswordSignUp)
+        val confirmationLayout = signUpView.findViewById<TextInputLayout>(R.id.txtLayoutConfirmPasswordSignUp)
+
         checkBox.setOnCheckedChangeListener{ _, isChecked ->
             btnSignUp.isEnabled = isChecked
         }
 
         btnSignUp.setOnClickListener {
-            val user = signUpView.findViewById<TextInputEditText>(R.id.txtEdtUsernameSIgnUp)
-            val password = signUpView.findViewById<TextInputEditText>(R.id.txtEdtPasswordSignUp)
-            val confirmation = signUpView.findViewById<TextInputEditText>(R.id.txtEdtConfirmPasswordSignUp)
-
+            var taSemErro = true
             if (user.text.toString().isEmpty()){
-                user.error = "Username is required"
-            } else if (password.text.toString().isEmpty()){
-                password.error = "Password is required"
-            } else if (confirmation.text.toString().isEmpty()){
-                confirmation.error = "Password confirmation is required"
-            } else if (!confirmation.text.toString().equals(password.text.toString())){
-                confirmation.error = "Password mismatch"
-            } else {
+                userLayout.error = "Username is required"
+                taSemErro = false
+            }
+            if (password.text.toString().isEmpty()){
+                passwordLayout.error = "Password is required"
+                taSemErro = false
+            }
+            if (confirmation.text.toString().isEmpty()){
+                confirmationLayout.error = "Password confirmation is required"
+                taSemErro = false
+            }
+            if (!confirmation.text.toString().equals(password.text.toString())){
+                confirmationLayout.error = "Password mismatch"
+                taSemErro = false
+            }
+            if (taSemErro){
                 iSignUp.signUp(user.text.toString(), password.text.toString(), confirmation.text.toString(), signUpView)
             }
         }
