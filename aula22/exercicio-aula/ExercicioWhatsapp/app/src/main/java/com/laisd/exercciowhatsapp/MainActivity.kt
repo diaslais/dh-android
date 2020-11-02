503a359e9d9de6cf94b7007a267beb12474f408d
Lais Dias <laisdias099@gmail.com>
2020-11-02 16:51:57 -0300

    add Adapter and edit viewpager style

--- aula22/exercicio-aula/ExercicioWhatsapp/app/src/main/java/com/laisd/exercciowhatsapp/MainActivity.kt
+++ aula22/exercicio-aula/ExercicioWhatsapp/app/src/main/java/com/laisd/exercciowhatsapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.laisd.exercciowhatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentChat = ChatFragment()
+        val fragmentStatus = StatusFragment()
+        val fragmentCalls = CallsFragment()
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        tabLayout.setupWithViewPager(viewPager)
+
+        val fragments = listOf(fragmentChat, fragmentStatus, fragmentCalls)
+
+        val titulos = listOf("Chats", "Status", "Calls")
+
+        viewPager.adapter = Adapter(fragments, titulos, supportFragmentManager)
     }
 }
